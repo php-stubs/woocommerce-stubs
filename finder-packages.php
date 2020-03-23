bca18500c11d4f2ebd744381c93347e6470a5192
Csaba Maulis <csaba@om4.com.au>
2020-03-23 17:09:06 +0800

    Add missing Action Scheduler stubs

--- finder-packages.php
+++ finder-packages.php
@@ -1,6 +1,18 @@
 <?php
 
 return \StubsGenerator\Finder::create()
-    ->in('source/woocommerce/packages/*/src')
-    ->sortByName()
+    ->in([
+        'source/woocommerce/packages/*/src',
+        'source/woocommerce/packages/action-scheduler/classes',
+        'source/woocommerce/packages/action-scheduler/deprecated',
+        'source/woocommerce/packages/action-scheduler/lib',
+        ])
+        ->append(
+            \StubsGenerator\Finder::create()
+                ->files()
+                ->depth('< 1')
+                ->name('functions.php')
+                ->in(['source/woocommerce/packages/action-scheduler'])
+        )
+        ->sortByName()
 ;
