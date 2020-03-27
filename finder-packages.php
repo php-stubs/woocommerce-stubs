ed61e54cd88d0c890fd3518152c3bd924e5047a4
Csaba Maulis <csaba@om4.com.au>
2020-03-28 05:06:19 +0800

    Reorganise finder
    Make clear package related includes and excludes

--- finder-packages.php
+++ finder-packages.php
@@ -3,24 +3,37 @@
 return \StubsGenerator\Finder::create()
     ->in([
         'source/woocommerce/packages/*/src',
-        'source/woocommerce/packages/action-scheduler/classes',
-        // Current classes extend deprecated ones.
-        'source/woocommerce/packages/action-scheduler/deprecated',
-
-        'source/woocommerce/packages/woocommerce-admin/includes',
     ])
+    // Action Scheduler.
     ->append(
         \StubsGenerator\Finder::create()
-            ->files()
-            ->depth('< 1')
-            ->name('functions.php')
-            ->in(['source/woocommerce/packages/action-scheduler'])
+            ->in([
+                'source/woocommerce/packages/action-scheduler/classes',
+                // Current classes extend deprecated ones.
+                'source/woocommerce/packages/action-scheduler/deprecated',
+            ])
+            ->append(
+                \StubsGenerator\Finder::create()
+                    ->files()
+                    ->depth('< 1')
+                    ->name('functions.php')
+                    ->in(['source/woocommerce/packages/action-scheduler'])
+            )
+            // Uses WP-CLI.
+            ->notPath('WP_CLI')
+            ->sortByName()
     )
-    // Uses WP-CLI.
-    ->notPath('WP_CLI')
-    // Exclude email templates.
-    ->notPath('emails')
-    // Update functions.
-    ->notPath('wc-admin-update-functions.php')
+    // WC Admin
+    ->append(
+        \StubsGenerator\Finder::create()
+            ->in([
+                'source/woocommerce/packages/woocommerce-admin/includes',
+            ])
+            // Email templates.
+            ->notPath('emails')
+            // Update functions for internal use.
+            ->notPath('wc-admin-update-functions.php')
+            ->sortByName()
+    )
     ->sortByName()
 ;
