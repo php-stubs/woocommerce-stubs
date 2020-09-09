397a934d5dfee5f52006197c7ea450d614882502
Viktor Szépe <viktor@szepe.net>
2020-09-09 17:20:50 +0000

    Generate stubs for WooCommerce 4.5.1

--- woocommerce-stubs.php
+++ woocommerce-stubs.php
@@ -32028,7 +32028,7 @@ namespace {
          *
          * @var string
          */
-        public $version = '4.5.0';
+        public $version = '4.5.1';
         /**
          * WooCommerce Schema version.
          *
