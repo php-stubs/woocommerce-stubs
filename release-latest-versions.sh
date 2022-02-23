b6c7807e47011fe9a7c3aaedd68e8e02cc501ba7
Viktor Szépe <viktor@szepe.net>
2022-02-23 09:22:25 +0000

    Upgrade to Composer v2

--- release-latest-versions.sh
+++ release-latest-versions.sh
@@ -15,7 +15,8 @@ WC_JSON="$(wget -q -O- "https://packagist.org/packages/woocommerce/woocommerce.j
 #for V in                     3.5 3.6 3.7 3.8 3.9 \
 #         4.0 4.1 4.2 4.3 4.4 4.5 4.6     4.8 4.9 \
 #         5.0 5.1 5.2 5.3 5.4 5.5 5.6 5.7 5.8 5.9; do
-for V in         5.5 5.6 5.7 5.8 5.9; do
+for V in                     5.5 5.6 5.7 5.8 5.9
+         6.0 6.1 6.2 6.3 6.4; do
     # Find latest version
     printf -v JQ_FILTER '.package.versions[].version | select(test("^%s\\\\.%s\\\\.\\\\d+$"))' "${V%.*}" "${V#*.}"
     LATEST="$(jq -r "$JQ_FILTER" <<<"$WC_JSON" | sort -t "." -k 3 -g | tail -n 1)"
