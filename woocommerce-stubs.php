054fd3cf90722978ace761d8bc3be51c55ea9f37
Viktor Szépe <viktor@szepe.net>
2025-07-21 17:54:17 +0000

    Generate stubs for WooCommerce 10.0.3

--- woocommerce-stubs.php
+++ woocommerce-stubs.php
@@ -38216,7 +38216,7 @@ namespace {
          *
          * @var string
          */
-        public $version = '10.0.2';
+        public $version = '10.0.3';
         /**
          * WooCommerce Schema version.
          *
