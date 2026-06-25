e6d583b9fa05b3f5ff67f86c939b45a0c35b8444
Viktor Szépe <viktor@szepe.net>
2026-06-25 10:47:57 +0000

    Generate stubs for WooCommerce 10.9.1

--- woocommerce-stubs.php
+++ woocommerce-stubs.php
@@ -39916,7 +39916,7 @@ namespace {
          *
          * @var string
          */
-        public $version = '10.9.0';
+        public $version = '10.9.1';
         /**
          * WooCommerce Schema version.
          *
