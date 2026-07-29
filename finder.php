ea9b2a6c6c4381a4d79786848182c035096a876f
Francesco <frosso@users.noreply.github.com>
2026-07-29 19:29:24 +0200

    Fix overly broad Api path exclusion (#33)

--- finder.php
+++ finder.php
@@ -12,7 +12,7 @@ return \StubsGenerator\Finder::create()
     ->append(
         \StubsGenerator\Finder::create()
             ->in(['source/woocommerce/src'])
-            ->notPath('Api')
+            ->notPath('#^Api(?:/|$)#')
             ->notPath('Internal')
             // Uses woocommerce/blueprint
             ->notPath('Admin/Features/Blueprint')
